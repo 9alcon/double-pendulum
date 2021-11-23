0ec11cf29cba7e371f84a0ca9140fbc8e94350c9
9alcon <89615069+9alcon@users.noreply.github.com>
2021-11-24 00:49:18 +0100

    Update double_pendulum.py

--- double_pendulum.py
+++ double_pendulum.py
@@ -8,28 +8,30 @@ pygame.init()
 height = 600
 width = 1000
 win = pygame.display.set_mode((width, height))
-r1 = r2 = 175
-m1 = m2 = 15
+r1 = r2 = 175#length of the rod
+m1 = m2 = 15#mass 1 and 2
 a1 = random.uniform(0, 2*pi)
 a2 = random.uniform(0, 2*pi)
-o = (width/2, 200)
+#a1 and a2 are the angles of r1 and r2 respectively
+o = (width/2, 200)#origin
 a1v = 0
 a2v = 0
-g = 1
-t = 0
-trail_limit = 400
+#angular acceleration
+g = 1#gravitational constant
+trail_limit = 400 #the max points that should be in trail list
 
 trail = list()
 
 run = True
+#run loop
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-    
+    #pop the first element of the list to keep it small
     if len(trail) > len(trail):
         trail.pop(0)
-
+    #math from https://www.myphysicslab.com/pendulum/double-pendulum-en.html
     num1 = -g * (2 * m1 + m2) * sin(a1)
     num2 = -m2 * g * sin(a1 - 2 * a2)
     num3 = -2 * sin(a1 - a2) * m2
@@ -43,17 +45,19 @@ while run:
     num4 = a2v * a2v * r2 * m2 * cos(a1 - a2)
     den = r2 * (2 * m1 + m2 - m2 * cos(2 * a1 - 2 * a2))
     a2a = (num1 * (num2 + num3 + num4)) / den
-
+    
+    #updating the position based on the angle
     x1 = r1 * sin(a1) + width/2
     y1 = r1 * cos(a1) + o[1]
     x2 = x1 + r2 * sin(a2)
     y2 = y1 + r2 * cos(a2)
-
+    #updating the velocity and position
     a1v += a1a
     a2v += a2a
     a1 += a1v
     a2 += a2v
-
+    
+    #drawing everything
     win.fill((255, 255, 255))
     trail.append((x2, y2))
     if len(trail) > 1:
@@ -65,4 +69,4 @@ while run:
     pygame.draw.circle(win, (0, 0, 0), (x2, y2), m2, 0)
     pygame.display.update()
     time.sleep(0.0250)
-    t += 0.0250
+    
